cb001cd8ba682ad734f9441415143a7357d2a70d
Manish <manish@Manishs-Air.attlocal.net>
2024-12-18 13:41:00 -0600

    Style Chanes

--- utils/backtoDiv.php
+++ utils/backtoDiv.php
@@ -7,7 +7,4 @@ if (isset($_SERVER['HTTP_REFERER'])) {
     $previous = 'default.php'; // Replace with your fallback URL
 }
 ?>
-<div>
-<a href="contact.html" >Back to Division</a>
-</div>
 <button onclick="window.location.href='<?php echo $previous; ?>'" class="btnback">Go Back</button>

c7c90a1602268ed1469fda0a78458d2ec0a59fe2
Manish <manish@Manishs-Air.attlocal.net>
2024-12-18 11:42:43 -0600

    Menu Update

--- utils/menu.php
+++ utils/menu.php
@@ -2,29 +2,29 @@
     <li class="active"><a class="navlink" href="index">Home</a></li>
     <li><a class="navlink" href="#">Engineering Divisions</a>
         <ul class="sub-menu">
-                        <li><a href="mechanical.html">Mechanical</a></li>
-                        <li><a href="instrumentation.html">Instrumentation</a></li>
-                        <li><a href="safety.html">Safety</a></li>
-                        <li><a href="consumables.html">Consumables</a></li>
-                        <li><a href="electrical.html">Electrical</a></li>
-                        <li><a href="communication.html">Communication</a></li>
-                        <li><a href="spares.html">Spares</a></li>
-                        <li><a href="general.html">General Items</a></li>
+                        <li><a href="mechanical">Mechanical</a></li>
+                        <li><a href="instrumentation">Instrumentation</a></li>
+                        <li><a href="safety">Safety</a></li>
+                        <li><a href="consumables">Consumables</a></li>
+                        <li><a href="electrical">Electrical</a></li>
+                        <li><a href="communication">Communication</a></li>
+                        <li><a href="spares">Spares</a></li>
+                        <li><a href="general">General Items</a></li>
 
                     </ul>
     </li>
     <li><a class="navlink" href="#">Product Portfolio</a>
         <ul class="sub-menu">
-            <li><a href="safety.html">Safety</a></li>
-            <li><a href="instrumentation.html">Instrumentation</a></li>                                        
-            <li><a href="valves.html">Valves, Pipes & Fittings</a></li>
-            <li><a href="general.html">General</a></li>   
+            <li><a href="safety">Safety</a></li>
+            <li><a href="instrumentation">Instrumentation</a></li>                                        
+            <li><a href="valves">Valves, Pipes & Fittings</a></li>
+            <li><a href="general">General</a></li>   
 
         </ul>
     </li>
     
     
 
-    <li><a class="navlink" href="contact.html">Contact Us</a></li>
+    <li><a class="navlink" href="contact">Contact Us</a></li>
     
 </ul>

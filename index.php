e4c76fee33360307fc75f6607e5efd812a69cee6
Manish <manish@Manishs-Air.attlocal.net>
2024-12-18 11:29:31 -0600

    Updating Menu to a single File

--- index.php
+++ index.php
@@ -67,36 +67,7 @@
                     </div>
                     <!-- Main Menu  -->
                     <div class="main-menu d-none d-lg-block">
-                        <ul>
-                            <li class="active"><a class="navlink" href="index.html">Home</a></li>
-                            <li><a class="navlink" href="#">Engineering Divisions</a>
-                                <ul class="sub-menu">
-                                                <li><a href="mechanical.html">Mechanical</a></li>
-                                                <li><a href="instrumentation.html">Instrumentation</a></li>
-                                                <li><a href="safety.html">Safety</a></li>
-                                                <li><a href="consumables.html">Consumables</a></li>
-                                                <li><a href="electrical.html">Electrical</a></li>
-                                                <li><a href="communication.html">Communication</a></li>
-                                                <li><a href="spares.html">Spares</a></li>
-                                                <li><a href="general.html">General Items</a></li>
-
-                                            </ul>
-                            </li>
-                            <li><a class="navlink" href="#">Product Portfolio</a>
-                                <ul class="sub-menu">
-                                    <li><a href="safety.html">Safety</a></li>
-                                    <li><a href="instrumentation.html">Instrumentation</a></li>                                        
-                                    <li><a href="valves.html">Valves, Pipes & Fittings</a></li>
-                                    <li><a href="general.html">General</a></li>   
-
-                                </ul>
-                            </li>
-                           
-                           
-
-                            <li><a class="navlink" href="contact.html">Contact Us</a></li>
-                          
-                        </ul>
+                        <?php include('utils/menu.php') ?>
                     </div>
 
                     <div class="header-right ">
